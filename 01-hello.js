13c1ea622c3074ccf4f23325e90bced75da1fcdb
Tshemekga <tramakau30@gmail.com>
2026-06-27 12:21:43 +0200

    broskiecodes
    Fix syntax errors and improve formatting in the JavaScript code.

--- 01-hello.js
+++ 01-hello.js
@@ -1,1 +1,21 @@
 javascript//My first JS file console.log("Hello from broskiecodes")
+//02-Calculator
+const a=10;
+const b=5;
+console.log("Add:"a+b);
+//03-Guess game code 
+JavaScript 
+//03-Guess Game
+console.log("---Guess Game---"):
+const secretNumber=
+  Math.floor(Math.random()*+10)+1;
+const guess=7;//Change this number to try different guesses
+console.log("I'm thinking of a number 1-10..."):
+if(guess===secretNumber)
+  console.log("You got it! The number was"7)
+else if 70
+console.log("Too low! The number was "7)
+console.log("Too high! The Number was",7)
+**How to play it**: Change 'const guess=7;to 1-10
+  Re-run in browser console later
+Math.random()'picks a new secret number every time
